a9698c94d8f2c963b93d0e7191d630bf6e5b2570
ze-fernando <zefernando.dev@gmail.com>
2024-12-18 15:28:50 -0300

    create: tables

--- Models/Image.cs
+++ Models/Image.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ImageRecognition.Models;
 
 public class Image{
+    
+    [Key]
     public required int Id {get; set; }
+    
     public required string ImageBase64 {get; set; }
+    
     public required string CustomerCode {get; set; }
+    
     public required string ImageUrl {get; set; }
+    
     public required string OcrResult {get; set; }
+    
     public required DateTime ProcessedA {get; set; }
 };
