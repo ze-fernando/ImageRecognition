d4ffa1d8fe41f78e9cfe59d57d6b1a7427d8bcb3
ze-fernando <zefernando.dev@gmail.com>
2024-12-18 22:18:21 -0300

    add: IA package

--- Config/Settings.cs
+++ Config/Settings.cs
@@ -4,4 +4,6 @@ namespace ImageRecognition.Config;
 
 public static class Settings{
     public static string Connection = Env.GetString("DATABAE_CONNECTION");
+
+    public static string GeminiKey = Env.GetString("GEMINI_KEY");
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
 using DotNetEnv;
+using DotnetGeminiSDK;
 using ImageRecognition.Config;
 using ImageRecognition.Entity;
+using ImageRecognition.Services;
 using Microsoft.EntityFrameworkCore;
 
 Env.Load();
@@ -9,6 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+              options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+builder.Services.AddScoped<ImageService>();
+builder.Services.AddScoped<GeminiService>();
+
+builder.Services.AddGeminiClient(config =>
+        {
+            config.ApiKey = Settings.GeminiKey;
+        });
 
 builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseNpgsql(Settings.Connection));
@@ -23,5 +39,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+// using (var scope = app.Services.CreateScope())
+// {
+//     var geminiService = scope.ServiceProvider.GetRequiredService<GeminiService>();
+//     await geminiService.Example();
+// }
+app.MapControllers();
 app.Run();
