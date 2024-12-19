f0121c2db56888d224ab217851315155a83327c1
ze-fernando <zefernando.dev@gmail.com>
2024-12-19 00:47:56 -0300

    create: migrations

---
+++ Migrations/20241219031312_ChangeTable.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageRecognition.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerCode",
+                table: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "ProcessedA",
+                table: "Images",
+                newName: "ProcessedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageBase64",
+                table: "Images",
+                newName: "CostumerCode");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MeasurementType",
+                table: "Images",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeasurementType",
+                table: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "ProcessedAt",
+                table: "Images",
+                newName: "ProcessedA");
+
+            migrationBuilder.RenameColumn(
+                name: "CostumerCode",
+                table: "Images",
+                newName: "ImageBase64");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerCode",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using ImageRecognition.Config;
 using ImageRecognition.Entity;
 using ImageRecognition.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 Env.Load();
 
@@ -22,13 +23,15 @@ builder.Services.AddScoped<ImageService>();
 builder.Services.AddScoped<GeminiService>();
 
 builder.Services.AddGeminiClient(config =>
-        {
-            config.ApiKey = Settings.GeminiKey;
-        });
+    {
+        config.ApiKey = Settings.GeminiKey;
+        config.ImageBaseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash"; 
+    });
 
 builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseNpgsql(Settings.Connection));
-
+    
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var app = builder.Build();
 
@@ -39,10 +42,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-// using (var scope = app.Services.CreateScope())
-// {
-//     var geminiService = scope.ServiceProvider.GetRequiredService<GeminiService>();
-//     await geminiService.Example();
-// }
+
+app.UseStaticFiles(
+    new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+        RequestPath = "/images"
+    });
 app.MapControllers();
 app.Run();
